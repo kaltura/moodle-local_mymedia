e8e4b0240818851af8ba5b8949fed92fa4886f5d
Tyler Bannister <tyler.bannister@remote-learner.net>
2014-10-03 16:22:58 -0400

    Updated to release 4.0.00

--- lti_launch.php
+++ lti_launch.php
@@ -1,4 +1,6 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
@@ -13,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * My Media version file.
+ * My Media LTI launch page.
  *
  * @package    local_mymedia
  * @author     Remote-Learner.net Inc
@@ -21,15 +23,29 @@
  * @copyright  (C) 2014 Remote Learner.net Inc http://www.remote-learner.net
  */
 
-if (!defined('MOODLE_INTERNAL')) {
-    die('Direct access to this script is forbidden.');
-}
+require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
+require_once(dirname(dirname(dirname(__FILE__))).'/local/kaltura/locallib.php');
 
-$plugin->version = 2014023000;
-$plugin->component = 'local_mymedia';
-$plugin->release = 'Kaltura release 4.0.00 (RL: 2.6.2.0)';
-$plugin->requires = 2013111801;
-$plugin->maturity = MATURITY_STABLE;
-$plugin->dependencies = array(
-    'local_kaltura' => 2014013000
-);
+global $USER;
+
+require_login();
+
+$context = context_user::instance($USER->id);
+require_capability('local/mymedia:view', $context);
+
+$launch = array();
+$launch['id'] = 1;
+$launch['cmid'] = 0;
+$launch['title'] = 'My Media';
+$launch['module'] = KAF_MYMEDIA_MODULE;
+$launch['course'] = $PAGE->course;
+$launch['width'] = '300';
+$launch['height'] = '300';
+$launch['custom_publishdata'] = '';
+
+if (local_kaltura_validate_mymedia_required_params($launch)) {
+    $content = local_kaltura_request_lti_launch($launch);
+    echo $content;
+} else {
+    echo get_string('invalid_launch_parameters', 'local_mymedia');
+}
